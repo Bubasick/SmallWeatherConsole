5e4b87da0be645b7694d82bf463c9fe024e39e3b
Danylo Kaminskyi <DanyloKam@gmail.com>
2021-07-08 13:43:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WeatherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather weather = GetWeather("https://goweather.herokuapp.com/weather/Kyiv");
+            Console.WriteLine(weather.Temperature);
+        }
+        public static Weather GetWeather(string uri)
+        {
+            WebRequest request = WebRequest.Create(uri);
+            WebResponse response = request.GetResponse();
+            Weather weatherForecast;
+            using (Stream data = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(data);
+
+                string responseFromServer = reader.ReadToEnd();
+
+                weatherForecast = JsonConvert.DeserializeObject<Weather>(responseFromServer);
+
+
+            }
+            response.Close();
+
+            return weatherForecast;
+
+        }
+    }
+    public class Weather
+    {
+        public String Temperature { get; set; }
+        public String Wind { get; set; }
+        public String Description { get; set; }
+        public List<Forecast> Forecast { get; set; }
+
+
+    }
+
+    public class Forecast
+    {
+        public int Day { get; set; }
+        public String Temperature { get; set; }
+        public String Wind { get; set; }
+    }
+}
